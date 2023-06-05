49eb807f235f92af5e25db87def0bb89280cf0de
memesnopower <115490701+memesnopower@users.noreply.github.com>
2023-06-05 12:21:54 +0300

    Create Group.h

---
+++ Group.h
@@ -0,0 +1,18 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+#include <unordered_set>
+#include <algorithm>
+#include <iostream>
+#include "Student.h"
+#include "Student.cpp"
+
+class Group {
+public:
+
+private:
+	std::string group_name;
+	std::unordered_set<Student, StudentH
+};
+
+#endif // GROUP_H
