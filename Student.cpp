12f54fc453e409ee081e2ba5b6c688f4c472c6bf
memesnopower <115490701+memesnopower@users.noreply.github.com>
2023-06-05 23:13:10 +0300

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -2,41 +2,61 @@
 
 Student::Student() = default;
 
-Student::Student(std::string name, std::string surname, std::string patronymic, size_t studentId) {
+Student::Student(const std::string& name, const std::string& surname, const std::string& patronymic, const size_t id) {
 	this->name = name;
 	this->surname = surname;
 	this->patronymic = patronymic;
-	this->studentId = studentId;
+	this->id = id;
 }
 
-void Student::add_discipline(Discipline ob) {
-	disciplines.push_back(ob);
+const std::string& Student::get_name() const {
+	return this->name;
 }
 
-double Student::get_average_mark() const {
-	double total = 0;
-	for (const auto& pos : disciplines) {
-		total += pos.mark;
+const std::string& Student::get_surname() const {
+	return this->surname;
+}
+
+const std::string& Student::get_patronymic() const {
+	return this->patronymic;
+}
+
+const size_t Student::get_id() const {
+	return this->id;
+}
+
+void Student::add_mark(const std::string& subject, size_t mark) {
+	marks[subject] = mark;
+}
+
+const std::map<std::string, size_t>& Student::get_marks() const {
+	return this->marks;
+}
+
+
+double Student::get_average() const {
+	size_t total = 0;
+	for (const auto& mark : marks) {
+		total += mark.second;
 	}
-
-	return total / disciplines.size();
+	
+	return static_cast<double>(total) / marks.size();
 }
 
-bool Student::operator<(const Student& ob) const {
-	return this->get_average_mark() < ob.get_average_mark();
+bool Student::operator==(const Student& ob) const {
+	return name == ob.name && surname == ob.surname && patronymic == ob.patronymic && id == ob.id;
 }
 
-
-bool Student::is_Excellent() const {
-	if (get_average_mark() == 5) {
+bool Student::isExcellent() const {
+	if (get_average() == 5) {
 		return true;
 	}
 	return false;
 }
 
-bool Student::is_Doubler() const {
-	for (const auto& pos : disciplines) {
-		if (pos.mark == 2) {
+bool Student::isDoubler() const {
+	for (const auto& pos : marks) {
+		if (pos.second == 2) {
 			return true;
 			break;
 		}
@@ -44,73 +64,34 @@ bool Student::is_Doubler() const {
 	}
 }
 
-Student::~Student() = default;
 
 
-std::string Student::get_name() const {
-	return this->name;
-}
+std::ostream& operator<<(std::ostream& os, const Student& student) {
+	os << student.name << std::endl;
+	os << student.surname << std::endl;
+	os << student.patronymic << std::endl;
+	os << student.id << std::endl;
 
-std::string Student::get_surname() const {
-	return this->surname;
-}
-
-
-std::string Student::get_patronymic() const {
-	return this->patronymic;
-}
-
-size_t Student::get_id() const {
-	return this->studentId;
-}
-
-std::vector<Discipline> Student::get_disciplines() const {
-	return disciplines;
-}
-
-void Student::set_name(const std::string& name) {
-	this->name = name;
-}
-
-void Student::set_surname(const std::string& surname) {
-	this->surname = surname;
-}
-
-void Student::set_patronymic(const std::string& patronymic) {
-	this->patronymic = patronymic;
-}
-
-void Student::set_id(const size_t id) {
-	this->studentId = studentId;
-}
-
-void Student::set_discipline(const std::vector<Discipline>& disciplines) {
-	this->disciplines = disciplines;
-}
-
-bool Student::operator==(const Student& s) const {
-	return name == s.name && surname == s.surname && patronymic == s.patronymic && studentId == s.studentId;
-}
-
-
-std::istream& operator>>(std::istream& is, Student& ob) {
-	std::getline(is, ob.name);
-	std::getline(is, ob.surname);
-	std::getline(is, ob.patronymic);
-	is >> ob.studentId;
-
-	// Добавить для оценок 
-
-	return is;
-}
-
-std::ostream& operator<<(std::ostream& os, const Student& ob) {
-	os << "Name: " << ob.name << std::endl;
-	os << "Surnamne: " << ob.surname << std::endl;
-	os << "Patronymic: " << ob.patronymic << std::endl;
-	os << "Student ID: " << ob.studentId << std::endl;
-
-	// Доделать для оценок
+	for (const auto& mark : student.marks) {
+		os << mark.first << " " << mark.second << std::endl;
+	}
 
 	return os;
+}
+
+std::istream& operator>>(std::istream& is, Student& student) {
+	is >> student.name;
+	is >> student.surname;
+	is >> student.patronymic;
+	is >> student.id;
+	size_t n = 0;
+	is >> n;
+	for (size_t i = 0; i < n; ++i) {
+		std::string subject;
+		size_t mark;
+		is >> subject >> mark;
+		student.add_mark(subject, mark);
+	}
+
+	return is;
 }
