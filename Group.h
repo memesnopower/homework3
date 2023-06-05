b07177cae8ac5a034d217e5117b0bef7d1ed8c55
memesnopower <115490701+memesnopower@users.noreply.github.com>
2023-06-05 12:32:27 +0300

    Update Group.h

--- Group.h
+++ Group.h
@@ -5,14 +5,21 @@
 #include <algorithm>
 #include <iostream>
 #include "Student.h"
-#include "Student.cpp"
 
 class Group {
 public:
+	Group();
+	Group(const std::string& group_name);
+
+	void add_student(Student &ob);
+	
+	friend std::istream& operator>>(std::istream& is, Group& ob);
+	friend std::ostream& operator<<(std::ostream& os, const Group& ob);
 
 private:
 	std::string group_name;
-	std::unordered_set<Student, StudentH
+	std::unordered_set<Student, StudentHasher> students;
 };
 
 #endif // GROUP_H
+
