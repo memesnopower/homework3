38c4ebe4109e9ac93d9f56d97502f5bd5eef0885
memesnopower <115490701+memesnopower@users.noreply.github.com>
2023-06-06 19:21:49 +0300

    Update Group.h

--- Group.h
+++ Group.h
@@ -1,6 +1,9 @@
+#ifndef GROUP_H
+#define GROUP_H
 
 #include "Student.h"
 #include <unordered_set>
+#include "Exceptions.h"
 #include <iostream>
 
 class Group {
@@ -9,7 +12,10 @@ public:
 	Group(const std::string& group_name);
 
 	void add_student(const Student& ob);
-	/*void remove_student(const std::string& name);*/
+	void remove_student(const size_t id);
+	void search_name(const std::string& name) const;
+	void search_surname(const std::string& surname) const;
+	void search_patronymic(const std::string& patronymic) const;
 
 	double getAverageMark() const;
 	double getAverageMarkBySubject(const std::string& subject) const;
